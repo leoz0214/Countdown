8f5f24df9f91e87f86b1cc036e76a3e890d9957a
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-10-01 11:25:05 +0100

    Solution settings user interface created

--- src/end.py
+++ src/end.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import secrets
+from typing import Literal
 
 import menu
 import game
@@ -9,6 +10,8 @@ import audio
 from colours import *
 from font import ink_free
 
+
+OPERATORS = "+-x÷"
 
 WINNING_MESSAGES = (
     "Amazing!", "Awesome!", "Brilliant!", "Congratulations!", "Excellent!",
@@ -47,6 +50,19 @@ STREAK_XP_MULTIPLIER = {
 
 ADD_XP_CHUNK_COUNT = 50
 ADD_XP_DELAY_MS = 40
+
+MIN_SOLUTION_NUMBERS_COUNT = 4
+MAX_SOLUTION_NUMBERS_COUNT = 7
+
+MIN_SOLUTIONS_COUNT = 1
+MAX_SOLUTIONS_COUNT = 100
+DEFAULT_SOLUTIONS_COUNT = 10
+
+SOLUTION_PARENTHESES_OPTIONS = ("Allow nested", "Disallow nested", "OFF")
+
+MIN_SOLUTIONS_SECONDS = 1
+MAX_SOLUTIONS_SECONDS = 60
+DEFAULT_SOLUTIONS_SECONDS = 10
 
 LEVEL_UP_SFX = audio.get_sfx("levelup.wav")
 
@@ -335,18 +351,183 @@ class SolutionsFrame(tk.Frame):
         self.root.title("Countdown - Finish - Solutions")
 
         self.title_label = tk.Label(
-            self, font=ink_free(100), text="Solutions")
+            self, font=ink_free(75, True), text="Solutions")
         self.selected_numbers_frame = game.SelectedNumbersFrame(
             self, self.numbers)
         self.target_number_label = game.TargetNumberLabel(self, self.target)
+        self.solutions_options_frame = SolutionsOptionsFrame(self)
         self.back_button = tk.Button(
             self, font=ink_free(25), text="Back", width=15, border=3,
             bg=ORANGE, activebackground=GREEN,
             command=self.game_end.exit_solutions)
 
-        self.title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        self.title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
         self.selected_numbers_frame.grid(
-            row=1, column=0, columnspan=2, padx=10, pady=10)
+            row=1, column=0, columnspan=2, padx=10, pady=5)
         self.target_number_label.grid(
             row=2, column=0, padx=10, pady=10, sticky="w")
-        self.back_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.solutions_options_frame.grid(row=2, column=1, padx=10, pady=5, sticky="w")
+        self.back_button.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+
+
+class SolutionsOptionsFrame(tk.Frame):
+    """
+    Allows the player to filter for particular types of solutions:
+    - Minimum and maximum number count
+    - Maximum solution count
+    - Whether to allow nested parentheses, parentheses only, or none
+    - Allowed operators (add/subtract/multiply/divide)
+
+    Also allows the player to set a time limit for how long to search
+    for solutions (rather slow with maximum settings...)
+    """
+
+    def __init__(self, master: SolutionsFrame) -> None:
+        super().__init__(master)
+        self.master = master
+
+        self.min_number_count_frame = SolutionNumberCountOFrame(self, "min")
+        self.max_number_count_frame = SolutionNumberCountOFrame(self, "max")
+        self.max_solution_count_frame = MaxSolutionsFrame(self)
+        self.parentheses_frame = SolutionParenthesesFrame(self)
+        self.operators_frame = SolutionOperatorsFrame(self)
+        self.seconds_limit_frame = SolutionsSecondsLimitFrame(self)
+
+        self.min_number_count_frame.pack(padx=10)
+        self.max_number_count_frame.pack(padx=10)
+        self.max_solution_count_frame.pack(padx=10)
+        self.parentheses_frame.pack(padx=10)
+        self.operators_frame.pack(padx=10)
+        self.seconds_limit_frame.pack(padx=10)
+    
+    def check_number_counts(self) -> None:
+        """
+        Ensures the maximum number count is increased to at least the
+        minimum number count if currently lower and that
+        the minimum number count is decreased to at least the
+        maximum number count if currently lower.
+        """
+        min_count = self.min_number_count_frame.count.get()
+        if self.max_number_count_frame.count.get() < min_count:
+            self.max_number_count_frame.count.set(min_count)
+            return
+        
+        max_count = self.max_number_count_frame.count.get()
+        if self.min_number_count_frame.count.get() > max_count:
+            self.min_number_count_frame.count.set(max_count)
+
+
+class SolutionNumberCountOFrame(tk.Frame):
+    """
+    Holds a number count setting (min/max).
+    """
+
+    def __init__(
+        self, master: SolutionsOptionsFrame, bound: Literal["min", "max"]
+    ) -> None:
+        super().__init__(master)
+        self.bound = bound
+        self.label = tk.Label(
+            self, font=ink_free(15, True),
+            text="{} number count:".format(
+                "Minimum" if self.bound == "min" else "Maximum"))
+        self.count = tk.IntVar(
+            self, MIN_SOLUTION_NUMBERS_COUNT if self.bound == "min"
+            else MAX_SOLUTION_NUMBERS_COUNT)
+        
+        self.count_scale = tk.Scale(
+            self, font=ink_free(15), length=200,
+            from_=MIN_SOLUTION_NUMBERS_COUNT, to=MAX_SOLUTION_NUMBERS_COUNT,
+            orient="horizontal", variable=self.count, sliderlength=50,
+            command=lambda _: self.master.check_number_counts())
+        
+        self.label.pack(side="left", padx=10)
+        self.count_scale.pack(padx=10)
+
+
+class MaxSolutionsFrame(tk.Frame):
+    """
+    Holds maximum number of solutions to generate setting.
+    """
+
+    def __init__(self, master: SolutionsOptionsFrame) -> None:
+        super().__init__(master)
+        self.count = tk.IntVar(self, DEFAULT_SOLUTIONS_COUNT)
+        self.label = tk.Label(
+            self, font=ink_free(15, True), text="Maximum solution count:")
+        
+        self.count_scale = tk.Scale(
+            self, font=ink_free(15), length=200, orient="horizontal",
+            from_=MIN_SOLUTIONS_COUNT, to=MAX_SOLUTIONS_COUNT,
+            variable=self.count)
+        
+        self.label.pack(side="left", padx=10)
+        self.count_scale.pack(padx=10)
+
+
+class SolutionParenthesesFrame(tk.Frame):
+    """
+    Holds parentheses settings for solution, either:
+    - Nested parentheses allowed
+    - Nestead parentheses disallowed
+    - No parentheses allowed at all
+    """
+
+    def __init__(self, master: SolutionsOptionsFrame) -> None:
+        super().__init__(master)
+        self.label = tk.Label(
+            self, font=ink_free(15, True), text="Parentheses:")
+        self.label.pack(side="left", padx=10)
+        # 1 - Nested, 0 - No nested, -1 - No parentheses
+        self.option = tk.IntVar(self, 1)
+
+        for option, value in zip(
+            SOLUTION_PARENTHESES_OPTIONS, range(1, -2, -1)
+        ):
+            radiobutton = tk.Radiobutton(
+                self, font=ink_free(15), text=option,
+                variable=self.option, value=value, selectcolor=ORANGE)
+            radiobutton.pack(side="left")
+
+
+class SolutionOperatorsFrame(tk.Frame):
+    """
+    Holds the setting for which operators a solution can have
+    i.e add/subtract/multiply/divide
+    """
+
+    def __init__(self, master: SolutionsOptionsFrame) -> None:
+        super().__init__(master)
+        self.label = tk.Label(
+            self, font=ink_free(15, True), text="Operators:")
+        self.label.pack(side="left", padx=10)
+
+        self.operators = {
+            operator: tk.IntVar(self, True) for operator in OPERATORS
+        }
+        for operator in OPERATORS:
+            checkbutton = tk.Checkbutton(
+                self, font=ink_free(15), text=operator,
+                variable=self.operators[operator], selectcolor=ORANGE)
+            checkbutton.pack(side="left", padx=5)
+
+
+class SolutionsSecondsLimitFrame(tk.Frame):
+    """
+    Holds the maximum number of seconds to generate solutions for.
+    """
+
+    def __init__(self, master: SolutionsOptionsFrame) -> None:
+        super().__init__(master)
+        self.label = tk.Label(
+            self, font=ink_free(15, True),
+            text="Maximum seconds to generate for:")
+        self.seconds = tk.IntVar(self, DEFAULT_SOLUTIONS_SECONDS)
+
+        self.seconds_scale = tk.Scale(
+            self, font=ink_free(15), length=200, orient="horizontal",
+            from_=MIN_SOLUTIONS_SECONDS, to=MAX_SOLUTIONS_SECONDS,
+            variable=self.seconds)
+        
+        self.label.pack(side="left", padx=10)
+        self.seconds_scale.pack(padx=10)
